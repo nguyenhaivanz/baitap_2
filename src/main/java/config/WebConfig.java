42f140155ba66c4b8b6a3c1f32df0345166e4066
nguyenhaivanz <haivan090824@gmail.com>
2022-11-18 15:21:13 +0700

    first commit

---
+++ src/main/java/config/WebConfig.java
@@ -0,0 +1,80 @@
+package config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring5.ISpringTemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import service.ProductService;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("controller")
+public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public ProductService productService(){
+        return new ProductService();
+    }
+
+    //    Thymeleaf Configuration
+    @Bean
+    public SpringResourceTemplateResolver templateResolver(){
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(applicationContext);
+        templateResolver.setPrefix("/WEB-INF/views/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        return templateResolver;
+    }
+
+    @Bean
+    public TemplateEngine templateEngine(){
+        TemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        return templateEngine;
+    }
+
+    @Bean
+    public ThymeleafViewResolver viewResolver(){
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setCharacterEncoding("UTF-8");
+        viewResolver.setTemplateEngine((ISpringTemplateEngine) templateEngine());
+        return viewResolver;
+    }
+
+    @Bean
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSizePerFile(90000000);
+        return multipartResolver;
+    }
+
+    // lấy thư mục tĩnh ở đâu;
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("file:D:\\module_3\\case_study_module_3_new\\BaiTap_1\\src\\main\\webapp\\WEB-INF/");
+
+    }
+}
---
+++ src/main/java/controller/ProductController.java
@@ -0,0 +1,64 @@
+package controller;
+
+
+import model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+import service.ProductService;
+
+import java.io.File;
+import java.io.IOException;
+
+@Controller
+public class ProductController {
+    @Autowired
+    ProductService productService;
+
+    @GetMapping("/products")
+    public ModelAndView getAll() {
+        ModelAndView modelAndView = new ModelAndView("/showProduct");
+        modelAndView.addObject("products", productService.products);
+        return modelAndView;
+    }
+
+    @GetMapping("/home")
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView("/home");
+        modelAndView.addObject("name", "Van");
+        return modelAndView;
+    }
+
+    @GetMapping("/delete/{id}")
+    public ModelAndView delete(@PathVariable int id) {
+        productService.delete(id);
+        ModelAndView modelAndView = new ModelAndView("redirect:/products");
+        return modelAndView;
+    }
+
+    @GetMapping("/create")
+    public String getCreate() {
+        return "createProduct";
+    }
+
+    @ModelAttribute(name = "toan")
+    public Product product(){
+        return new Product();
+    }
+
+    @PostMapping("/create")
+    public String create(Product product,@RequestParam MultipartFile upImg) {
+        String name = upImg.getOriginalFilename();
+        try {
+            upImg.transferTo(new File("/D:/module_3/case_study_module_3_new/BaiTap_1/src/main/webapp/WEB-INF/img/"+ name));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        product.setImg("/img/"+ name);
+        productService.add(product);
+        return "redirect:/products";
+    }
+}
